ea733b7de947affd185124e7ae3c6b7817dd5f28
MerdiMOKE028 <69848159+MerdiMOKE028@users.noreply.github.com>
2023-09-16 10:43:56 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,20 +1,24 @@
 <! DOCTYPE Html>
 
 <html>
-<head>
-    <title> emploietservice</title>
-</head>
-<body>
 
-    <!-- Menu connexion-->
-    <?php include 'menunavigation.php'; ?>
-
-    <form method="post">
-        
-        </form>
-    
-
-
-
-    </body>
-    </html>>
+ <head>
+ <meta charset="utf-8">
+ <!-- importer le fichier de style -->
+ <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+ </head>
+ <body style='background:#fff;'>
+ <div id="content">
+ <!-- tester si l'utilisateur est connecté -->
+ <?php
+ session_start();
+ if($_SESSION['username'] !== ""){
+ $user = $_SESSION['username'];
+ // afficher un message
+ echo "Bonjour $user, vous êtes connecté";
+ }
+ ?>
+ 
+ </div>
+ </body>
+</html>
